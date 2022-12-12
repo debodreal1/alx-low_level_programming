4a8036d9eee9d6c047ba5917c1cfc59e97802686
debodreal1 <debodreal1@gmail.com>
2022-12-12 15:05:32 -0800

    print como

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
 /**
  * main - prints all possible different combinations of two digits
- *
  * Return: Always 0 (success)
- *
  */
 
 int main(void)
@@ -12,7 +9,7 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	
+
 	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
@@ -29,9 +26,7 @@ int main(void)
 			}
 		}
 	}
-	
 	putchar('\n');
 
-	return (0);
-
+return (0);
 }
