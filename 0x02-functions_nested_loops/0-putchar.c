bf9bbfd84f675e9df6dfd212574c16dd3ad6ec35
debodreal1 <debodreal1@gmail.com>
2022-12-13 08:08:35 -0800

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,22 @@
 
 /**
  *main - Entry point
- *
  * Return: Always 0(Success)
  * /
- 
+
  int main(void)
+{
+:
 
- {
- 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
-	return (0);
+return (0);
 }
 
